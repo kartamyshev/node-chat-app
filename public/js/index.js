9f60f9bb08d953bf933cc29531ac91ca8dd56dd5
kartamyshev <kartamyshev.k@yandex.ru>
2018-03-31 11:22:48 +0200

    test crossbrowser message

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,18 @@ socket.on('connect', function () {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage ->', message);
+  const div = document.createElement('div');
+  div.innerHTML = `<li>${message.from} ${message.text} ${message.createdAt}</li>`;
+  document.body.appendChild(div);
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+
+function sendMessage() {
+  socket.emit('createMessage', {
+    from: 'Mike',
+    text: 'Heroku'
+  });
+}
