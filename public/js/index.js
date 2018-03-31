1c38cba1476207d086600675184c93ec7a2f7a90
kartamyshev <kartamyshev.k@yandex.ru>
2018-03-31 11:09:31 +0200

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,11 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'alla@gmail.com',
-    text: 'Hey. This is Alla. Everything seems to work great!'
-  });
 });
 
 socket.on('newMessage', function(message) {
   console.log('newMessage ->', message);
 });
-
-
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
