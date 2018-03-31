6e1471edd1723380aac3190a294ffda43ce015a5
kartamyshev <kartamyshev.k@yandex.ru>
2018-03-31 11:40:40 +0200

    creet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'Welcome to the chat app!',
+    createdAt: Date.now()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'New user joined!',
+    createdAt: Date.now()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage ->', message);
     io.emit('newMessage', {
@@ -21,6 +32,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
